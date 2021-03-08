34a5f9c031f77968acfc1789e8a06ed34d47e4b4
cydry3 <ysaita09@gmail.com>
2021-03-08 23:40:46 +0900

    Drop comments.

--- init.el
+++ init.el
@@ -5,17 +5,9 @@
 
 ;; Customizing with GUI.
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (tango-dark)))
  '(package-selected-packages (quote (twittering-mode emmet-mode web-mode mozc))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
 
 ;; Presentation.
